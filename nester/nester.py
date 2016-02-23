b174bd506b0c57884ba86479f580ffbc2d561408
RayYJWu <ray60110@gmail.com>
2016-02-23 23:51:16 +0800

    nester_1.3.0

--- nester/nester.py
+++ nester/nester.py
@@ -1,9 +1,12 @@
 #A function for printing words and words in a list.
 
-def print_lol(the_list):
+def print_lol(the_list, indent=False, level=0):
     for each_item in the_list:
         if isinstance(each_item, list):
-            print_lol(each_item)
+            print_lol(each_item, indent, level+1)
         else:
-            print(each_list)
+            if indent:
+                for tab_stop in range(level):
+                    print('\t', end='')
+            print(each_item)
         
--- nester/setup.py
+++ nester/setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name= 'nester',
-    version='1.0.0',
+    version='1.3.0',
     py_modules= ['nester'],
     author='ray60110',
     author_email='ray60110@gmail.com',
