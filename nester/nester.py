c4ac3878657043cd8ebb2587cc00bf119a6c0883
RayYJWu <ray60110@gmail.com>
2016-02-23 23:42:46 +0800

    nester
    contain print_lol function and the first distribution modules

---
+++ nester/nester.py
@@ -0,0 +1,9 @@
+#A function for printing words and words in a list.
+
+def print_lol(the_list):
+    for each_item in the_list:
+        if isinstance(each_item, list):
+            print_lol(each_item)
+        else:
+            print(each_list)
+        
---
+++ nester/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(
+    name= 'nester',
+    version='1.0.0',
+    py_modules= ['nester'],
+    author='ray60110',
+    author_email='ray60110@gmail.com',
+    url='...',
+    )
