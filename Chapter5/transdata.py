d79a3dfca55fd9f18b0d187553c3f1885b4c21ce
RayYJWu <ray60110@gmail.com>
2016-02-26 18:26:31 +0800

    Added files via upload

--- Chapter5/transdata.py
+++ Chapter5/transdata.py
@@ -1,42 +1,34 @@
 def sanitize(time_string):
-    if '-' in record:
+    if '-' in time_string:
         splitter= '-'
-    elif ':' in record:
+    elif ':' in time_string:
         splitter= ':'
     else:
-        return(time_string)
+        return time_string
     (mins, secs)= time_string.split(splitter)
     return(mins+'.'+secs)
-
-clean_jam=[]
-clean_jul=[]
-clean_mike=[]
-clean_sara=[]
 
 try:
     with open('james.txt', 'r') as jam:
         record= jam.readline()
         james= record.strip().split(',')
-        for jamesItem in james:
-            clean_jam.append(sanitize(jamesItem))
     with open('julie.txt', 'r') as jul:
         record= jul.readline()
         julie= record.strip().split(',')
-        for julieItem in julie:
-            clean_jul.append(sanitize(julieItem))
     with open('mikey.txt', 'r') as mike:
         record= mike.readline()
         mikey= record.strip().split(',')
-        for mikeyItem in mikey:
-            clean_mike.append(sanitize(mikeyItem))
     with open('sarah.txt', 'r') as sara:
         record= sara.readline()
         sarah= record.strip().split(',')
-        for sarahItem in sarah:
-            clean_sara.append(sanitize(sarahItem))
+
 except IOError as ioerr:
     print('error', str(ioerr))
 
+clean_jam=[sanitize(each_jam) for each_jam in james]
+clean_jul=[sanitize(each_jul) for each_jul in julie]
+clean_mike=[sanitize(each_mike) for each_mike in mikey]
+clean_sara=[sanitize(each_sara) for each_sara in sarah]
 
 print(sorted(clean_jam), end='\n')
 print(sorted(clean_jul), end='\n')
